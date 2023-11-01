f792890b051a6342346293f42fa87e39e93e083b
apoj <angeltobonperez5@gmail.com>
2023-10-31 22:50:35 -0500

    beta comun

---
+++ Api.ADAproductosComun.Comun/Enums/GeneralEnum.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.ADAproductosComun.Comun.Enums
+{
+    public class GeneralEnum
+    {
+        public GeneralEnum() { }
+
+        public enum TipoRetorno
+        {
+            OK,
+            NOK,
+            ERROR,
+            ADVERTENCIA
+        }
+
+    }
+}
---
+++ Api.ADAproductosComun.Comun/Retorno.cs
@@ -0,0 +1,27 @@
+﻿using Api.ADAproductosComun.Comun.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.ADAproductosComun.Comun
+{
+    public class Retorno<T>
+    {
+        public Retorno() { }
+
+        public bool Estado { get; set; }
+
+        public T Informacion { get; set;}
+
+        public GeneralEnum.TipoRetorno TipoRetorno { get; set; }
+
+        public DateTime FechaConsulta { get; set; }
+
+        public List<String> Mensaje { get; set; }
+
+
+
+    }
+}
